08ac68d49e16bb40e11e36dbda5f3c6f89605f19
Subramanyam Kandlagunta <subramanyamk2003@gmail.com>
2025-03-28 10:39:18 +0530

    use case 6

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -1,12 +1,15 @@
 import random
 
 position = 0  # Position of Single Player
+total_rolls = 0
 
 
 def snake_and_ladder():
     global position
+    global total_rolls
     while position != 100:
         outcome = random.randint(1, 6)
+        total_rolls += 1
         option = random.randint(1, 3)  # 1 - NoPlay, 2 - Ladder, 3 - Snake
         if option == 2:
             position += outcome
@@ -17,6 +20,10 @@ def snake_and_ladder():
             position -= outcome
             if position < 0:
                 position = 0
+
+        print(f"The position after {total_rolls} rolls is: {position}")
+
+    print(f"The total rolls to win: {total_rolls}")
 
 
 if __name__ == "__main__":
