e366b850eee8ecc3058327ef13a011c7d72f308d
Subramanyam Kandlagunta <subramanyamk2003@gmail.com>
2025-03-28 10:30:34 +0530

    use case 4

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -5,12 +5,16 @@ position = 0  # Position of Single Player
 
 def snake_and_ladder():
     global position
-    outcome = random.randint(1, 6)
-    option = random.randint(1, 3)  # 1 - NoPlay, 2 - Ladder, 3 - Snake
-    if option == 2:
-        position += outcome
-    elif option == 3:
-        position -= outcome
+    while position != 100:
+        outcome = random.randint(1, 6)
+        option = random.randint(1, 3)  # 1 - NoPlay, 2 - Ladder, 3 - Snake
+        if option == 2:
+            position += outcome
+
+        elif option == 3:
+            position -= outcome
+            if position < 0:
+                position = 0
 
 
 if __name__ == "__main__":
